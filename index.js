757e5291c96d563eaf2b682de77460297f5c4cc9
Radee khan <khanradee318@gmail.com>
2024-12-16 12:53:56 -0500

    fixed css bugs

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let runButton = document.getElementById('run');
 let quizCodeEntry = document.getElementById('quizCode')
 
 runButton.addEventListener('click', () => {
-    location.href = "InQuiz/quiz.html"
+    location.href = "quiz.html"
     localStorage.setItem('quizCode', quizCodeEntry.value)
 })
